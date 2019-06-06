4b486a006f5d983760ecc2230b9285d3f6a0ed9f
Joe Casabona <jcasabona@gmail.com>
2019-06-06 14:27:36 -0400

    5:2 Code
    Enable admin columns/edits

--- taxonomies/register.php
+++ taxonomies/register.php
@@ -8,6 +8,8 @@ function lil_register_size_taxonomy() {
 	$args = array( 
 		'labels' => $labels,
 		'public' => true,
+		'show_admin_column' => true,
+		'show_in_quick_edit' => true,
 	);
 
 	$post_types = array( 'business' );
