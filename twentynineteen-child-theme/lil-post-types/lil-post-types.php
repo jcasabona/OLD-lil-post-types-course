937a2706b2bc2e90aa6074878a6769ae698872c9
Joe Casabona <jcasabona@gmail.com>
2019-06-08 12:24:16 -0400

    Branch name change

--- twentynineteen-child-theme/lil-post-types/lil-post-types.php
+++ twentynineteen-child-theme/lil-post-types/lil-post-types.php
@@ -4,10 +4,6 @@ define( 'LIL_VERSION', '1.0.0' );
 define( 'LILDOMAIN', 'lil-post-types' );
 define( 'LILPATH', get_stylesheet_directory() . '/lil-post-types/');
 
-// End of Video: "setting up our plugin"
-
-// Start of 4:2
-
 require_once( LILPATH . '/post-types/register.php' );
 
 add_action( 'init', 'lil_register_business_type' );
