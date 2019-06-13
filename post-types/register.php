68f8e48feab03320e91cfe8b72608625241d8962
Joe Casabona <jcasabona@gmail.com>
2019-06-13 10:04:08 -0400

    Updating to match new lesson

--- post-types/register.php
+++ post-types/register.php
@@ -1,24 +1,22 @@
-<?php 
+<?php
+
 function lil_register_business_type() {
+    $labels = array( 
+        'name' => __( 'Businesses', LILDOMAIN ),
+        'singular_name' => __( 'Business', LILDOMAIN ),
+        'featured_image' => __( 'Business Logo', LILDOMAIN ),
+        'set_feature_image' => __( 'Set Business Logo', LILDOMAIN ),
+        'remove_feature_image' => __( 'Remove Logo', LILDOMAIN ),
+        'use_feature_image' => __( 'Use Logo', LILDOMAIN ),
+        'archives' => __( 'Business Directory', LILDOMAIN ),
+        'add_new' => __( 'Add New Business', LILDOMAIN ),
+        'add_new_item' => __( 'Add New Business', LILDOMAIN ),
+    );
 
-	$labels = array(
-		'name' => __( 'Businesses', LILDOMAIN ),
-		'singular_name' => __( 'Business', LILDOMAIN ),
-		'featured_image' => __( 'Business Logo', LILDOMAIN ),
-		'set_featured_image' => __( 'Set Business Logo', LILDOMAIN ),
-		'remove_featured_image' => __( 'Remove Logo', LILDOMAIN ),
-		'use_featured_image' => __( 'Use Logo', LILDOMAIN ),
-		'archives' => __( 'Business Directory', LILDOMAIN ),
-	);
+    $args = array( 
+        'labels' => $labels,
+        'public' => true,
+    );
 
-	$args = array( 
-		'labels' => $labels,
-		'public' => true,
-		'has_archive' => 'businesses',
-		'rewrite' => array( 'has_front' => true ),
-		'menu_icon' => 'dashicons-building',
-		'supports' => array( 'title', 'editor', 'thumbnail' ),
-	);
-
-	register_post_type( 'business', $args );
+    register_post_type( 'business', $args );
 }
